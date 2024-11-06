69a84b841cc5cf4cd9d3e293aca575e2cd312ea6
digbanmije18@gmail.com <digbanmije18@gmail.com>
2024-11-06 06:24:59 +0100

    updating auth

--- goalFE/src/pages/screen/LandingPageScreen.tsx
+++ goalFE/src/pages/screen/LandingPageScreen.tsx
@@ -1,5 +1,17 @@
+import img from "../../assets/TASK.jpg";
+import img2 from "../../assets/image2.png";
+
 const LandingPageScreen = () => {
-  return <div className="bg-red-50 p-10 h-screen">LandingPageScreen</div>;
+  return (
+    <div
+      style={{
+        backgroundImage: `url(${img})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+      className="bg-red-50 w-full h-[30rem]"
+    ></div>
+  );
 };
 
 export default LandingPageScreen;
